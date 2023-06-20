b5cbb524aa2f416e1a16ee92ac15af13036287f3
SerjioAkapulka <vaginserg.96@mail.ru>
2023-06-20 16:13:42 +0500

    logging added

--- src/main/java/some/App.java
+++ src/main/java/some/App.java
@@ -1,6 +1,7 @@
 package some;
 
 import actor.NotificationActor;
+import actor.Printer;
 import akka.actor.typed.ActorRef;
 import akka.actor.typed.ActorSystem;
 import msg.EmailRequest;
@@ -10,10 +11,16 @@ import msg.SMSRequest;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("jnger");
         ActorRef<Request> ref = ActorSystem.create(NotificationActor.behavior(), "notificatioActor");
         ref.tell(new SMSRequest("789453465673"));
-        ref.tell(new EmailRequest("message1"));
+        ref.tell(new EmailRequest("123@mail.ru"));
+
+        // note that system is also the ActorRef to the guardian actor
+        final ActorRef<Printer.PrintMe> ref2 = ActorSystem.create(Printer.create(), "printer-sample-system");
+
+// these are all fire and forget
+        ref2.tell(new Printer.PrintMe("message 1"));
+        ref2.tell(new Printer.PrintMe("message 2"));
 
     }
 }
